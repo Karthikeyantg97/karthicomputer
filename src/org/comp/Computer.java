ffe7acafcb8dea14b7099cca6172c3954eeb8724
karthikeyantg97 <karthikeyantg97@gmail.com>
2023-01-05 13:26:02 +0530

    msg3

--- src/org/comp/Computer.java
+++ src/org/comp/Computer.java
@@ -5,6 +5,13 @@ public class Computer {
 		// TODO Auto-generated method stub
 System.out.println("Working");
 	}
+	private void hardDisk() {
+		System.out.println("800 GB");
+	}
+private void sizeOfRam() {
+	System.out.println("6GB");
+
+}
 	private void software() {
 	System.out.println("Software");
 }
